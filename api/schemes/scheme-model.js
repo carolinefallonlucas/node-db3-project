7bd7da961dcda84f0e8977ce3ff01e7987f9f6a1
Caroline <carolinefallonlucas@gmail.com>
2021-01-27 18:17:47 -0500

    scheme model success

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,28 @@
 // scheme-model
+const db = require("../../data/db-config")
+
+module.exports = {
+    find()
+    { 
+        return db("schemes");
+
+    }, 
+    findById(id)
+    {
+        return db("schemes")
+            .where("id", id)
+            .first(); 
+    },
+
+    add(scheme)
+    {
+        return db("schemes")
+        .insert(scheme)
+            .then(([scheme_id]) => 
+            {
+                return db("schemes")
+                    .where("id", scheme_id)
+                    .first();
+        })
+    }
+}
